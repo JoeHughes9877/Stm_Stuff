653e7366b4cee58b8fd488c001f1f1cf835e1b3d
JoeHughes9877 <joeydarlo1000@gmail.com>
2026-04-06 05:40:47 +0100

    write string

--- Clock/Core/Drivers/uart.c
+++ Clock/Core/Drivers/uart.c
@@ -5,7 +5,12 @@ void uart_init(void) {
       platform_uart_init();
 }
 
-void uart_write(const char *str) {}
+void uart_write(const char *str) {
+    while (*str) {
+        platform_uart_write_byte((uint8_t)(*str));
+        str++;
+    };
+}
 
 void uart_write_byte(uint8_t byte) {
     platform_uart_write_byte(byte);
